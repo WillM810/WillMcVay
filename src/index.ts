8f2814570824614b14e523c0fd1bd2d70005132f
Will McVay <mcvay.will@gmail.com>
2025-08-06 23:26:53 -0400

    Serve static files from the frontend build directory

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from 'express';
+import path from 'path';
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('/api', (req, res) => {
   res.send('Live!');
+});
+
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
 });
 
 app.listen(8080, () => {
