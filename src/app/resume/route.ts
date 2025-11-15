19668c0b817d2f73079d62318c992320bdf03296
Will McVay <mcvay.will@gmail.com>
2025-11-14 20:32:07 -0500

    move resume pdf to hide from direct access

--- src/app/resume/route.ts
+++ src/app/resume/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import fs from "fs";
 
 export async function GET() {
-  return new NextResponse(fs.readFileSync("public/William McVay - Full Stack Developer.pdf"), {
+  return new NextResponse(fs.readFileSync("src/assets/William McVay - Full Stack Developer.pdf"), {
     headers: {
       "Content-Type": "application/pdf",
       "Content-Disposition": 'inline; filename="William McVay - Full Stack Developer.pdf"',
