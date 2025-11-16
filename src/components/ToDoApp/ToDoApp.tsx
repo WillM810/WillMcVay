b8421c83f663b841f1acead94798c5d0b5740b70
Will McVay <mcvay.will@gmail.com>
2025-11-16 15:18:42 -0500

    todo improvements

--- src/components/ToDoApp/ToDoApp.tsx
+++ src/components/ToDoApp/ToDoApp.tsx
@@ -2,6 +2,8 @@
 
 import { useState } from "react";
 import ToDoItem from "./ToDoItem";
+import TextInput from "../Utils/TextInput";
+import PrimaryButton from "../Utils/PrimaryButton";
 
 export type ToDo = {
   done: boolean;
@@ -24,44 +26,27 @@ export default function ToDoApp() {
   return (
     <div className="p-10">
       <h1 className="font-bold text-2xl mb-2">To Do List</h1>
-      <div>
-        <input
-          type="text"
-          className="w-3xl mr-4 border-b border-gray-300 focus:border-blue-500 focus:outline-none py-2"
-          placeholder="New To Do"
+      <div className="flex justify-between items-center mb-6">
+        <TextInput
           id="new-todo-input"
-          onChange={e => onNewTodoChange(e)}
+          placeholder="New To Do"
           value={newTodo}
+          onChange={(e) => onNewTodoChange(e)}
         />
-        <button
+        <PrimaryButton
           onClick={handleSubmit}
-          className="
-            px-4
-            py-2
-            bg-blue-600
-            text-white
-            font-bold
-            text-3xl
-            rounded-lg
-            shadow-md
-            hover:bg-blue-700
-            focus:outline-none
-            focus:ring-2
-            focus:ring-blue-400
-            focus:ring-opacity-75
-            transition-colors
-          "
+          disabled={newTodo.trim().length === 0}
         >
           <svg className="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 4v16m8-8H4"></path>
           </svg>
-        </button>
-        <ul>
-          {todos.map((todo, index) => (
-            <ToDoItem key={todo.id} todo={todo} setTodos={setTodos} />
-          ))}
-        </ul>
+        </PrimaryButton>
       </div>
+      <ul>
+        {todos.map((todo, index) => (
+          <ToDoItem key={todo.id} todo={todo} setTodos={setTodos} />
+        ))}
+      </ul>
     </div>
   );
 }
