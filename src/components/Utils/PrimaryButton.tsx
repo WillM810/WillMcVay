64d7da1d28b94f3e2dc32c7eb32ad9479cfaf9d9
Will McVay <mcvay.will@gmail.com>
2025-11-16 11:45:02 -0500

    add utility components

---
+++ src/components/Utils/PrimaryButton.tsx
@@ -0,0 +1,31 @@
+type PrimaryButtonProps = {
+  onClick: () => void;
+  disabled?: boolean;
+  children: React.ReactNode;
+};
+
+export default function PrimaryButton(props: PrimaryButtonProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+      className="
+        px-4
+        py-2
+        bg-blue-600
+        text-white
+        rounded-lg
+        shadow-md
+        hover:bg-blue-700
+        focus:outline-none
+        focus:ring-2
+        focus:ring-blue-400
+        focus:ring-opacity-75
+        disabled:bg-gray-300
+        disabled:cursor-not-allowed
+        disabled:text-gray-600
+        transition-colors
+      "
+    >{!props.disabled ? props.children : '...'}</button>
+  );
+}
---
+++ src/components/Utils/TextInput.tsx
@@ -0,0 +1,25 @@
+type TextInputProps = {
+  id: string;
+  value: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onEnter?: () => void;
+  placeholder?: string;
+};
+
+export default function TextInput(props: TextInputProps) {
+  return (    
+    <input
+      type="text"
+      className="w-3xl mr-4 border-b border-gray-300 focus:border-blue-500 focus:outline-none py-2"
+      placeholder={props.placeholder}
+      id={props.id}
+      onChange={e => props.onChange(e)}
+      onKeyDown={e => {
+        if (e.key === 'Enter' && props.onEnter) {
+          props.onEnter();
+        }
+      }}
+      value={props.value}
+    />
+  );
+}
