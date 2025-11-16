551c27f7f77e750faafa96ffa208ef3c357f3af3
Will McVay <mcvay.will@gmail.com>
2025-11-16 11:44:11 -0500

    MapEmbed component

---
+++ src/components/MapEmbed/MapEmbed.tsx
@@ -0,0 +1,22 @@
+type MapEmbedProps = {
+  lat: number;
+  lon: number;
+};
+
+export default function MapEmbed({ lat, lon }: MapEmbedProps) {
+  const mapSrc = `https://www.google.com/maps?q=${lat},${lon}&z=15&output=embed`;
+
+  return (
+    <div className="w-full h-64 rounded overflow-hidden shadow">
+      <iframe
+        title="Location Map"
+        src={mapSrc}
+        width="100%"
+        height="100%"
+        allowFullScreen
+        loading="lazy"
+        className="border-0"
+      ></iframe>
+    </div>
+  );
+}
