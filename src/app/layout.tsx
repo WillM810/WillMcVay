0b98b5b8fb4900ac9506f5b534a3d9c22419925e
Will McVay <mcvay.will@gmail.com>
2026-06-05 23:03:02 -0400

    css module support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
 import "../styles/index.css";
 import "../styles/Resume.css";
 
